79fff54c8ddb6bbeb080955d10f1f90baf55493a
Toon Brand <toonbrand@gmail.com>
2016-04-08 15:02:19 +0200

    Veranderende css

--- src/nl/youngcapital/demo/FormulierOpzet.java
+++ src/nl/youngcapital/demo/FormulierOpzet.java
@@ -13,32 +13,43 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/a")
 public class FormulierOpzet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public FormulierOpzet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	static String theme;
+
+	public void init(){
+		this.theme = "theme1";
+	}
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public FormulierOpzet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("<html><head><title>hallo</title></head><link rel=stylesheet href=style.css><body>");
-//		String[] boodschappen = new String[4];
-//		boodschappen[0] = "Taart";
-//		boodschappen[1] = "Bloemkool";
-//		boodschappen[2] = "Aardappel";
-//		boodschappen[3] = "Paard";
-//		
-//		for(String product : boodschappen){
-//			response.getWriter().append("" + product + "<br>");
-//		}
-		response.getWriter().append(" <form method=get action=b><label for=user>Username:</label><input type=text name=user><br><label for=pwd>Password:</label><input type=password name=pwd><br><input type=submit class=button value=submit></form>");
-		response.getWriter().append(" <form method=get action=asdf><label for=status>Status code:<br></label><input type=radio name=status value=200>200<br><input type=radio name=status value=400>400<br><input type=radio name=status value=404>404<br><input type=radio name=status value=418>418<br><input type=radio name=status value=500>500<br><input type=submit class=button value=check></form>");
-		response.getWriter().append(" <form method=get action=d><label for=val1>Value1:</label><input type=text name=val1><br><select name=choice><option value=minus>Minus</option><option value=plus>Plus</option></select></br><label for=val2>Value2:</label><input type=text name=val2><br><input type=submit class=button value=calculate></form>");
+		response.getWriter().append("<html><head><title>hallo</title></head>");
+
+		if(request.getParameter("theme") != null){
+			theme = request.getParameter("theme");
+		}
+
+		switch(theme){
+		case "theme1" : response.getWriter().append("<link rel=stylesheet href=style.css><body>");
+		break;
+		case "theme2" : response.getWriter().append("<link rel=stylesheet href=style2.css><body>");
+		}
+
+		
+
+		response.getWriter().append("<form method=get ><label for=theme>Theme:</label><select name=theme><option value=theme1>Theme1</option><option value=theme2>Theme2</option></select><br><input type=submit class=button value=set theme></form>");
+
+		response.getWriter().append("<form method=get action=b><label for=user>Username:</label><input type=text name=user><br><label for=pwd>Password:</label><input type=password name=pwd><br><input type=submit class=button value=submit></form>");
+		response.getWriter().append("<form method=get action=asdf><label for=status>Status code:<br></label><input type=radio name=status value=200>200<br><input type=radio name=status value=400>400<br><input type=radio name=status value=404>404<br><input type=radio name=status value=418>418<br><input type=radio name=status value=500>500<br><input type=submit class=button value=check></form>");
+		response.getWriter().append("<form method=get action=d><label for=val1>Value1:</label><input type=text name=val1><br><select name=choice><option value=minus>Minus</option><option value=plus>Plus</option></select></br><label for=val2>Value2:</label><input type=text name=val2><br><input type=submit class=button value=calculate></form>");
 		response.getWriter().append("</body></html>");
 	}
 
